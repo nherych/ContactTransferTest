7a1b75f90e06eb4bd393173b38fe4590ea646de0
nherych <nherych@binariks.com>
2021-02-02 12:40:09 +0200

    initial commit

---
+++ ContactTransferTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactTransferTest
+//
+//  Created by Nazar Herych on 02.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
